321dff8d8000568ff533d2cb50944be52b85600b
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-06-19 13:42:51 +0530

    deployement

--- blog/settings.py
+++ blog/settings.py
@@ -131,7 +131,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
